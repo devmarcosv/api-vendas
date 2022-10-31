852e2f090b751d9c8297ebcd3ff97c3ed7e562b0
Marcos <devmarcosvinicius@gmail.com>
2022-10-31 10:38:34 -0400

    fix typeorm version

---
+++ src/shared/typeorm/migrations/1667227075620-CreateProducts.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateProducts1667227075620 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
